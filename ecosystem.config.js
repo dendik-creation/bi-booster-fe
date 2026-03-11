c61fe47b5d9f9d9905870c4de5063c21e6d9608d
dendik-creation <setiawandendik0205@gmail.com>
2026-03-11 12:27:18 +0700

    [fix] change script ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "bi-booster-fe",
-      script: "npm",
-      args: "run start",
+      script: "./node_modules/next/dist/bin/next",
+      args: "start",
       instances: 1,
       exec_mode: "fork",
       env: {
